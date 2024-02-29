044b01edf2a4bb1546495582d414f2fdcc1fc9c3
Ivanov Ilya <gergen892@gmail.com>
2024-02-29 12:57:19 +0500

    created Resources

---
+++ M16/Resources.swift
@@ -0,0 +1,24 @@
+//
+//  Resources.swift
+//  M16
+//
+//  Created by Илья Иванов on 29.02.2024.
+//
+
+import Foundation
+import UIKit
+
+enum Constants{
+    
+    enum Fonts{
+        static let font1 = UIFont.systemFont(ofSize: 14)
+    }
+    
+    enum Colors{
+        static let colorBlack = UIColor.black
+    }
+    
+    enum Texts{
+        static let textMain = "This is Main Text"
+    }
+}
